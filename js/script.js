7179a36d419d5b9ca860e3ddef1140c28db72f0e
Lazar <lazarstamenic83@gmail.com>
2019-08-16 08:05:30 +0200

    finished responsive layout at 320px width

--- js/script.js
+++ js/script.js
@@ -5,11 +5,22 @@ $( document ).ready(function() {
             $( ".yearly" ).addClass( "bill-bolded" );
             $( ".monthly" ).removeClass( "bill-bolded" );
         }else{
- 
-            console.log('565665656474747');
             $( ".yearly" ).removeClass( "bill-bolded" );
             $( ".monthly" ).addClass( "bill-bolded" );
         }
     });
-      
+
+        // burger
+    $('#nav-icon1').click(function(){
+	    $(this).toggleClass('open');
+    });
+       
+    $( "#nav-icon1" ).click(function() {
+        $( "nav" ).stop().toggleClass('nav-menu-open');
+    }); 
+        // burger
+
+    if ($(window).width() < 319) {
+        $('.commercial-holder p').text('DESKTOP 300 x 250');
+    }
 });
